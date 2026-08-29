0fd392ca7aaa522d8c4678558ecd34255563aab0
s6711423108-art <s6711423108@pkru.ac.th>
2026-08-29 14:26:25 +0700

    Update mqtt-plugin.js

--- mqtt-plugin.js
+++ mqtt-plugin.js
@@ -42,19 +42,31 @@ function mqttDatasource(settings, updateCallback) {
     var self = this;
     var currentSettings = settings;
     var client = null;
+    var reconnectTimer = null;
+
+    function makeClientId() {
+        var chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+        var id = "fb";
+        for (var i = 0; i < 10; i++) {
+            id += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+        return id;
+    }
 
     function connect() {
         if (client) {
             try { client.disconnect(); } catch (e) {}
         }
 
-        var clientId = "freeboard_" + Math.random().toString(16).substr(2, 10);
+        var clientId = makeClientId();
         client = new Paho.MQTT.Client(currentSettings.server, Number(currentSettings.port), clientId);
 
         client.onConnectionLost = function (responseObject) {
             if (responseObject.errorCode !== 0) {
-                console.log("MQTT connection lost: " + responseObject.errorMessage);
+                console.log("MQTT connection lost: " + responseObject.errorMessage + " -- retrying in 3s");
             }
+            if (reconnectTimer) clearTimeout(reconnectTimer);
+            reconnectTimer = setTimeout(connect, 3000);
         };
 
         client.onMessageArrived = function (message) {
@@ -71,12 +83,17 @@ function mqttDatasource(settings, updateCallback) {
         client.connect({
             useSSL: currentSettings.use_ssl,
             timeout: 10,
+            keepAliveInterval: 30,
+            mqttVersion: 4,
+            cleanSession: true,
             onSuccess: function () {
                 console.log("MQTT connected, subscribing to " + currentSettings.topic);
-                client.subscribe(currentSettings.topic);
+                client.subscribe(currentSettings.topic, { qos: 0 });
             },
             onFailure: function (e) {
-                console.log("MQTT connect failed: " + e.errorMessage);
+                console.log("MQTT connect failed: " + e.errorMessage + " -- retrying in 3s");
+                if (reconnectTimer) clearTimeout(reconnectTimer);
+                reconnectTimer = setTimeout(connect, 3000);
             }
         });
     }
@@ -86,6 +103,7 @@ function mqttDatasource(settings, updateCallback) {
     };
 
     this.onDispose = function () {
+        if (reconnectTimer) clearTimeout(reconnectTimer);
         if (client) {
             try { client.disconnect(); } catch (e) {}
         }
